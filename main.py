44a5d6a04f6320728d0125b1ddad583c3f5c3775
gab <gabrielesorci.25@gmail.com>
2021-12-07 09:00:29 +0100

    fix .gitignore and create new folder class

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 import sys
-from gdrive import GDrive
+from gdrive.gdrive import GDrive
 
 
-class Main:
+print("GSYNCPY - GOOGLE SYNC TOOL")
+print("")
 
-    def func(self, args):
-        print("GSYNCPY - GOOGLE SYNC TOOL")
-        print("")
 
-        if "--help" in args:
-            help()
-        elif "--upload" in args:
-            GDrive.upload()
-        elif "--download" in args:
-            GDrive.download()
-        else:
-            print("Error: invalid arguments, use --help for documentation")
+def main():
+    args = sys.argv
+    del args[0]
+    args = " ".join([str(elem) for elem in args])
 
-    def help():
-        print("help")
+    if "--help" in args:
+        help()
+    elif "--upload" in args:
+        GDrive.upload()
+    elif "--download" in args:
+        GDrive.download()
+    else:
+        print("Error: invalid arguments, use --help for documentation")
 
 
 if __name__ == "__main__":
-    args = sys.argv
-    del args[0]
-
-    str_args = " ".join([str(elem) for elem in args])
-
-    Main.func(str_args)
+    main()
