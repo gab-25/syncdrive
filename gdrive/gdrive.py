a2b862d62491da6a66e0a54e9ef06a0e85cabcd1
gab <gabrielesorci.25@gmail.com>
2021-12-24 10:04:38 +0100

    help dinamic list

--- gdrive/gdrive.py
+++ gdrive/gdrive.py
@@ -1,8 +1,11 @@
 class GDrive:
     idDrive: int
 
-    def __init__(self, idDrive):
+    def __init__(self, idDrive, args):
         self.idDrive = idDrive
+        for i, arg in enumerate(args):
+            if ("--" in arg):
+                getattr(GDrive, arg.replace("--", ""))
 
     def upload():
         """upload file into google drive"""
--- gdrive/helpers.py
+++ gdrive/helpers.py
@@ -1,5 +1,34 @@
-def help(commands):
-    print("HELP DOC - Command list of the tool")
+import os
+import glob
+import inspect
+import gdrive
 
-    for cmd in commands:
-        print(cmd.value)
+
+class Helpers:
+
+    @staticmethod
+    def help(args: str):
+        """list all commands tool"""
+        print("HELP DOC - Command list of the tool")
+        print()
+        Helpers.commands()
+
+    @staticmethod
+    def commands():
+        cmd_labels = []
+        path = os.path.dirname(os.path.abspath(__file__))
+        for file in glob.glob(os.path.join(path, "*.py")):
+            file_name = os.path.splitext(os.path.basename(file))[0]
+            module = getattr(gdrive, file_name)
+            class_attr = getattr(module, dir(module)[0])
+            class_func = inspect.getmembers(class_attr, inspect.isfunction)
+            for func in class_func:
+                if func[0] == "__init__" or func[1].__doc__ is None:
+                    continue
+
+                label = "--{0}  {1}".format(func[0], func[1].__doc__)
+                cmd_labels.append(label)
+
+        cmd_labels.sort()
+        for cmd in cmd_labels:
+            print(cmd)
