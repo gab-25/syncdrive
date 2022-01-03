e3fbbb4a11f65d3efffd148ed5016109ade6f2dd
gab <gabrielesorci.25@gmail.com>
2022-01-03 17:17:26 +0100

    fix

--- gdrive/gdrive.py
+++ gdrive/gdrive.py
@@ -1,8 +1,12 @@
+from selenium import webdriver
+
+
 class GDrive:
     idDrive: int
 
     def __init__(self, idDrive, args):
         self.idDrive = idDrive
+        GDrive.__login()
         for arg in args:
             try:
                 getattr(GDrive, arg)()
@@ -10,6 +14,8 @@ class GDrive:
                 print("Param {0} not found, use --help for list params".format(arg))
 
     def __login():
+        driver = webdriver.Firefox()
+        driver.get("https://accounts.google.com/signin")
         pass
 
     def upload():
