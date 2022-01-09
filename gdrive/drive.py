25c1e40b68a86a0c396620dbd74aaf6053b5bc48
gab <gabrielesorci.25@gmail.com>
2022-01-09 11:40:15 +0100

    rename gdrive

---
+++ gdrive/drive.py
@@ -0,0 +1,22 @@
+from login import Login
+
+
+class Drive:
+    idDrive: int
+
+    def __init__(self, idDrive: int, args: list):
+        self.idDrive = idDrive
+        Login()
+        for arg in args:
+            try:
+                getattr(Drive, arg)()
+            except AttributeError:
+                print("Param {0} not found, use --help for list params".format(arg))
+
+    def upload():
+        """upload: upload file into google drive"""
+        print("upload function")
+
+    def download():
+        """download: download file from google drive"""
+        print("download function")
