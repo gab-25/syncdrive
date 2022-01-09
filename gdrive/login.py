dcaf3d452e8d196e4162410facc57a2e8e79047a
gab <gabrielesorci.25@gmail.com>
2022-01-09 11:52:16 +0100

    fix

--- gdrive/login.py
+++ gdrive/login.py
@@ -1,13 +1,4 @@
-from __future__ import print_function
-
 import getpass
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 
 class User:
@@ -21,8 +12,6 @@ class User:
 
 class Login:
     user: User
-
-    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
     def __init__(self, user: User = None):
         if user is None:
@@ -43,38 +32,4 @@ class Login:
         }
 
     def __check_credts(self):
-        creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', self.SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-
-        try:
-            service = build('drive', 'v3', credentials=creds)
-
-            # Call the Drive v3 API
-            results = service.files().list(
-                pageSize=10, fields="nextPageToken, files(id, name)").execute()
-            items = results.get('files', [])
-
-            if not items:
-                print('No files found.')
-                return
-            print('Files:')
-            for item in items:
-                print(u'{0} ({1})'.format(item['name'], item['id']))
-        except HttpError as error:
-            # TODO(developer) - Handle errors from drive API.
-            print(f'An error occurred: {error}')
+        pass
