1348eb704ff857c0561dab766da0761c83ec27bd
gab <gabrielesorci.25@gmail.com>
2022-01-09 11:33:24 +0100

    add login

--- syncpy/login.py
+++ syncpy/login.py
@@ -1,4 +1,13 @@
+from __future__ import print_function
+
 import getpass
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class User:
@@ -13,6 +22,8 @@ class User:
 class Login:
     user: User
 
+    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
     def __init__(self, user: User = None):
         if user is None:
             print("GOOGLE LOGIN")
@@ -22,6 +33,7 @@ class Login:
 
         self.user = user
 
+        Login.__check_credts(self)
         Login.__singin(self)
 
     def __singin(self):
@@ -29,3 +41,40 @@ class Login:
             "Email": self.user.email,
             "Password": self.user.password
         }
+
+    def __check_credts(self):
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        try:
+            service = build('drive', 'v3', credentials=creds)
+
+            # Call the Drive v3 API
+            results = service.files().list(
+                pageSize=10, fields="nextPageToken, files(id, name)").execute()
+            items = results.get('files', [])
+
+            if not items:
+                print('No files found.')
+                return
+            print('Files:')
+            for item in items:
+                print(u'{0} ({1})'.format(item['name'], item['id']))
+        except HttpError as error:
+            # TODO(developer) - Handle errors from drive API.
+            print(f'An error occurred: {error}')
